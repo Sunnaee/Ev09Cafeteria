3ea937a91a452d7b9a8be7154e472cc254835324
emiunu <e.jaramillo02@ufromail.cl>
2024-11-19 19:21:53 -0300

    Subiendo clase CafeteriaController con codigo generado por VisualParadigm

---
+++ src/main/java/controller/CafeteriaController.java
@@ -0,0 +1,57 @@
+package controller;
+
+import model.*;
+
+public class CafeteriaController {
+
+	/**
+	 * 
+	 * @param cafeteria
+	 */
+	public static Cafeteria cargarDatos(Cafeteria cafeteria) {
+		// TODO - implement CafeteriaController.cargarDatos
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param cafeteria
+	 * @param cafe
+	 */
+	public static Cafeteria agregarCafe(Cafeteria cafeteria, Cafe cafe) {
+		// TODO - implement CafeteriaController.agregarCafe
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param cafeteria
+	 * @param nombre
+	 */
+	public static void descontinuarCafe(Cafeteria cafeteria, String nombre) {
+		// TODO - implement CafeteriaController.descontinuarCafe
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param cafeteria
+	 */
+	public static void guardarDatos(Cafeteria cafeteria) {
+		// TODO - implement CafeteriaController.guardarDatos
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param cafeteria
+	 * @param nombreNuevo
+	 * @param direccionNueva
+	 * @param redSocialNueva
+	 */
+	public static void modficarCafeteria(Cafeteria cafeteria, String nombreNuevo, String direccionNueva, String redSocialNueva) {
+		// TODO - implement CafeteriaController.modficarCafeteria
+		throw new UnsupportedOperationException();
+	}
+
+}
